3791fb26f8cb67c591ec150068307e027758dd0b
masoudvrz <masoud.varzi@yahoo.com>
2023-02-23 17:56:58 +0330

    feat : confing http server

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+// we confing our server here (different from express app cofing)
+const http = require('http');
+//  express app just a middleware top of the built-in node http server that has own middwares and route
+const app = require('./app');
+
+// server administrator can specify which port it should run on as environmental variable
+// || : if left side is false or undefined => right side is the main value;
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+server.listen(PORT , ()=> {
+
+})
+
+
