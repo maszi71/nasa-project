939f3db2e01c737ed4f8207927a96251946811b6
masoudvrz <masoud.varzi@yahoo.com>
2023-03-02 17:56:51 +0330

    feat: install cors package and configure for server

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,19 @@
 // config express app here different from server config like http
 const express = require('express');
+const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 // incoming request will pass throught JSON middleware
 app.use(express.json());
+// enable cors for all incoming requests
+// app.use(cors());
 
+// enablle for only our origin
+//Access-Control-Allow-Origin: http://localhost:3000
+app.use(cors({
+    origin : "http://localhost:3000"
+}))
 app.use(planetsRouter);
 
 module.exports = app;
